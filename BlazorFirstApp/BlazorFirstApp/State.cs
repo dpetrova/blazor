7d58f54400eec25b3091ea0a80b7666923e0c130
dpetrova <daniella_petrova@abv.bg>
2022-09-13 16:36:19 +0300

    form submit and validation

--- BlazorFirstApp/BlazorFirstApp/State.cs
+++ BlazorFirstApp/BlazorFirstApp/State.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorFirstApp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFirstApp
 {
     public class State
     {
         public string Role { get; set; } = "Admin";
+
+        [MaxLength(5)]
+        public string Author { get; set; } = "Pesho";
     }
 }
