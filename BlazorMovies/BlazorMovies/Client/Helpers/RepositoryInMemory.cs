95445a45ba816fd1f325d3758196e7984a6766b9
dpetrova <daniella_petrova@abv.bg>
2022-09-19 19:56:40 +0300

    render content with RenderFragment; use lifycycle methods; create and register services and use them through dependency injection system

---
+++ BlazorMovies/BlazorMovies/Client/Helpers/RepositoryInMemory.cs
@@ -0,0 +1,17 @@
+﻿using BlazorMovies.Shared.Entities;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class RepositoryInMemory : IRepository
+    {
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie(){Title = "Star Wars: A new hope", ReleaseDate = new DateTime(1977, 05, 25)},
+                new Movie(){Title = "Star Wars: Empire strikes back", ReleaseDate = new DateTime(1980, 05, 21)},
+                new Movie(){Title = "Star Wars: Return of the jedi", ReleaseDate = new DateTime(1983, 05, 25)},
+            };
+        }
+    }
+}
--- BlazorMovies/BlazorMovies/Client/Program.cs
+++ BlazorMovies/BlazorMovies/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorMovies.Client;
+using BlazorMovies.Client.Helpers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//configure services in the dipendency injection (DI) system
+//that way we will be able to request instances of these objects through DI (use @inject in components)
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); //Scoped: lives within a context (e.g. during an HTTP request)
+builder.Services.AddSingleton<SingletonService>(); //Singleton: single instance of the service is created
+builder.Services.AddTransient<TransientService>(); //Transient: different instances of the service are created each time that service is requested
+builder.Services.AddTransient<IRepository, RepositoryInMemory>(); //if IRepository is requested, then the DI system will reply with the instance of RepositoryInMemory class
 
 await builder.Build().RunAsync();
