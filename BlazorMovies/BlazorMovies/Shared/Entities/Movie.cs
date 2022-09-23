bf4401c10419cbe170ce3c681fda59c5de25689e
dpetrova <daniella_petrova@abv.bg>
2022-09-23 11:21:37 +0300

    call JS from C# and C# from JS; using cascading parameters; show confirm dialog before delete movie

--- BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -9,7 +9,7 @@ namespace BlazorMovies.Shared.Entities
     public class Movie
     {
         public string Title { get; set; }
-
         public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; }
     }
 }
