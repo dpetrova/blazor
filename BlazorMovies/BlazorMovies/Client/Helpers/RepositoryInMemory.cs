18841af5396323f4a43fd568e0000b31379fe448
dpetrova <daniella_petrova@abv.bg>
2022-09-24 20:37:32 +0300

    routing

--- BlazorMovies/BlazorMovies/Client/Helpers/RepositoryInMemory.cs
+++ BlazorMovies/BlazorMovies/Client/Helpers/RepositoryInMemory.cs
@@ -8,9 +8,9 @@ namespace BlazorMovies.Client.Helpers
         {
             return new List<Movie>()
             {
-                new Movie(){Title = "Star Wars: A new hope", ReleaseDate = new DateTime(1977, 05, 25)},
-                new Movie(){Title = "Star Wars: Empire strikes back", ReleaseDate = new DateTime(1980, 05, 21)},
-                new Movie(){Title = "Star Wars: Return of the jedi", ReleaseDate = new DateTime(1983, 05, 25)},
+                new Movie(){Id=1, Title = "Star Wars: A new hope", ReleaseDate = new DateTime(1977, 05, 25), Poster="https://m.media-amazon.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_FMjpg_UX1000_.jpg"},
+                new Movie(){Id= 2, Title = "Star Wars: The Empire strikes back", ReleaseDate = new DateTime(1980, 05, 21), Poster="https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"},
+                new Movie(){Id= 3, Title = "Star Wars: Return of the jedi", ReleaseDate = new DateTime(1983, 05, 25), Poster="https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"},
             };
         }
     }
--- BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -8,8 +8,25 @@ namespace BlazorMovies.Shared.Entities
 {
     public class Movie
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Poster { get; set; }
+        public string TitleBrief
+        {
+            get {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return null;
+                }
+
+                if(Title.Length > 60)
+                {
+                    return Title.Substring(0, 60) + "...";
+                }
+
+                return Title; 
+            }
+        }
     }
 }
